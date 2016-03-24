b2d961298425b9eae944d13913c65e222f65b7a1
Youri Tolstoy <ytolstoy@gmail.com>
2016-03-24 23:39:08 +0100

    Enhancing buffer usage by using UInt32LE for the size of the dataset and removeing the 65536 useless buffer

--- test/message-parsing.test.js
+++ test/message-parsing.test.js
@@ -2,98 +2,105 @@ var assert = require('assert'),
     net = require('net'),
     JsonSocket = require('../lib/json-socket');
 
-describe('JsonSocket message parsing', function() {
+describe('JsonSocket message parsing', function () {
 
     var socket = new JsonSocket(new net.Socket());
     var messages = [];
-    socket.on('message', function(message) {
+    socket.on('message', function (message) {
         messages.push(message);
     });
 
-    beforeEach(function() {
+    beforeEach(function () {
         messages = [];
-        socket._contentLength = null;
-        socket._buffer = new Buffer(0);
     });
 
-    it('should parse JSON strings', function() {
-        socket._handleData(new Buffer('13#"Hello there"'));
+    it('should parse JSON strings', function () {
+        socket._handleData(socket._formatMessageData("Hello there"));
         assert.equal(messages.length, 1);
         assert.equal(messages[0], 'Hello there');
-        assert.equal(socket._buffer, '');
     });
 
-    it('should parse JSON numbers', function() {
-        socket._handleData(new Buffer('5#12.34'));
+    it('should parse JSON numbers', function () {
+        socket._handleData(socket._formatMessageData('12.34'));
         assert.equal(messages.length, 1);
         assert.equal(messages[0], 12.34);
-        assert.equal(socket._buffer, '');
     });
 
-    it('should parse JSON bools', function() {
-        socket._handleData(new Buffer('4#true'));
+    it('should parse JSON bools', function () {
+        socket._handleData(socket._formatMessageData(true));
         assert.equal(messages.length, 1);
         assert.equal(messages[0], true);
-        assert.equal(socket._buffer, '');
     });
 
-    it('should parse JSON objects', function() {
-        socket._handleData(new Buffer('17#{"a":"yes","b":9}'));
+    it('should parse JSON objects', function () {
+        socket._handleData(socket._formatMessageData({"a": "yes", "b": 9}));
         assert.equal(messages.length, 1);
         assert.deepEqual(messages[0], {a: 'yes', b: 9});
-        assert.equal(socket._buffer, '');
     });
 
-    it('should parse JSON arrays', function() {
-        socket._handleData(new Buffer('9#["yes",9]'));
+    it('should parse JSON arrays', function () {
+        socket._handleData(socket._formatMessageData(["yes", 9]));
         assert.equal(messages.length, 1);
         assert.deepEqual(messages[0], ['yes', 9]);
-        assert.equal(socket._buffer, '');
     });
 
-    it('should parse multiple messages in one packet', function() {
-        socket._handleData(new Buffer('5#"hey"4#true'));
+    it('should parse multiple messages in one packet', function () {
+        socket._handleData(Buffer.concat([socket._formatMessageData("hey"), socket._formatMessageData(true)]));
         assert.equal(messages.length, 2);
         assert.equal(messages[0], 'hey');
         assert.equal(messages[1], true);
-        assert.equal(socket._buffer, '');
     });
 
-    it('should parse chunked messages', function() {
-        socket._handleData(new Buffer('13#"Hel'));
-        socket._handleData(new Buffer('lo there"'));
+    it('should parse chunked messages', function () {
+        var msgSize = Buffer.byteLength('"Hello there"');
+        var b = new Buffer(4 + Buffer.byteLength('"Hell'));
+        b.writeUInt32LE(msgSize);
+        b.write('"Hell', 4);
+        var c = new Buffer('o there"');
+
+        socket._handleData(b);
+        socket._handleData(c);
         assert.equal(messages.length, 1);
         assert.equal(messages[0], 'Hello there');
-        assert.equal(socket._buffer, '');
     });
 
-    it('should parse chunked and multiple messages', function() {
-        socket._handleData(new Buffer('13#"Hel'));
-        socket._handleData(new Buffer('lo there"4#true'));
+    it('should parse chunked and multiple messages', function () {
+        var msgSize = Buffer.byteLength('"Hello there"');
+        var b = new Buffer(4 + Buffer.byteLength('"Hell'));
+        b.writeUInt32LE(msgSize);
+        b.write('"Hell', 4);
+        var c = new Buffer('o there"');
+
+        //console.log('par', b.toString());
+        socket._handleData(b);
+        //console.log('par', Buffer.concat([c, socket._formatMessageData(true)]).toString());
+        socket._handleData(Buffer.concat([c, socket._formatMessageData(true)]));
         assert.equal(messages.length, 2);
         assert.equal(messages[0], 'Hello there');
         assert.equal(messages[1], true);
-        assert.equal(socket._buffer, '');
     });
 
-    it('should fail to parse invalid JSON', function() {
+    it('should fail to parse invalid JSON', function () {
+        var msgSize = Buffer.byteLength('"Hel');
+        var b = new Buffer(4 + msgSize);
+        b.writeUInt32LE(msgSize);
+        b.write('"Hel', 4);
         try {
-            socket._handleData(new Buffer('4#"Hel'));
+            socket._handleData(b);
         } catch (err) {
             assert.equal(err.code, 'E_INVALID_JSON');
         }
         assert.equal(messages.length, 0);
-        assert.equal(socket._buffer, '');
     });
 
-    it('should not accept invalid content length', function() {
-        try {
-            socket._handleData(new Buffer('wtf#"Hello"'));
-        } catch (err) {
-            assert.equal(err.code, 'E_INVALID_CONTENT_LENGTH');
-        }
-        assert.equal(messages.length, 0);
-        assert.equal(socket._buffer, '');
-    });
+    //it('should not accept invalid content length', function () {
+    //    try {
+    //        socket._handleData(socket._formatMessageData('wtf#"Hello"'));
+    //    } catch (err) {
+    //        assert.equal(err.code, 'E_INVALID_CONTENT_LENGTH');
+    //    }
+    //    assert.equal(messages.length, 0);
+    //    assert.equal(socket._buffer, '');
+    //});
 
 });
